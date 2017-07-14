e754d063f0ed441a28940e6da220d423a3413db3
Abhijeet Prasad <AbhiPrasad@users.noreply.github.com>
2017-07-13 21:51:22 -0400

    Update codeBreak.ino

--- codeBreak.ino
+++ codeBreak.ino
@@ -5,12 +5,13 @@ const int BUTTONWHITE = 12; // 3
 const int BUTTONINTER = 2; // Interrupt Pin for Colored Buttons
 
 int pressArr[4] = {0, 0, 0, 0}; //array that is pressed
-int randomArr[4] = {0, 0, 0, 0}; //randomized array that contains values that need to be checked
+int randomArr[4] = {1, 1, 1, 1}; //randomized array that contains values that need to be checked
 
 int correct = 0; //amount correct from array
 volatile int btnPress = -1;
 int error = 0;
 volatile int lastPress = 0;
+volatile int color = 0;
 
 bool pressed = false;
 
@@ -19,42 +20,62 @@ void setup() {
   // cli();
   Serial.begin(9600); // debugging purposes\
 
+  //interrupt buttons
+  pinMode(BUTTONINTER, INPUT);
+
   //Interrupt
   attachInterrupt(digitalPinToInterrupt(BUTTONINTER), btnLedPress, RISING);
 
-  //interrupt buttons
-  pinMode(BUTTONINTER, INPUT);
+  //scores
+  pinMode(BUTTONRED, OUTPUT);
+  pinMode(BUTTONGREEN, OUTPUT);
+  pinMode(BUTTONBLUE, OUTPUT);
+  pinMode(BUTTONWHITE, OUTPUT);
   
   //analog read
   pinMode(A5, INPUT_PULLUP);
 
   //seeds random function
   randomSeed(analogRead(0));
-  randomize();
+  //randomize();
 }
 
 void loop() {
   if (pressed) {
+    btnPressFlash();
+    delay(1000);
+    noInterrupts();
+    //Serial.println(color);
     //Serial.println(btnPress);
-    if (btnPress == 3) {
+    Serial.print("button press: ");
+    Serial.println(btnPress);
+    if (btnPress == 3 ) {
+      pressArr[btnPress] = lastPress;
       btnPress = -1;
-      /*
+      for (int i = 0; i < 4; i++) {
+        Serial.print(pressArr[i]);
+      }
+      Serial.println("");
+      for (int i = 0; i < 4; i++) {
+        Serial.print(randomArr[i]);
+      }
+      Serial.println("");
       bool sameArray = compareArray();
       if (sameArray) {
-        handleServo();
-        resetCode();
+        //handleServo();
+        //resetCode();
+        Serial.println("Same array!");
       } else {
         error = error + 1;
+        Serial.println("Wrong array");
       }
-      */
     } else {
       //Serial.println(lastPress);
       pressArr[btnPress] = lastPress;
     }
     pressed = false;
-  }
-
-  if (error == 5) {
+    interrupts();
+  } else if (error == 5) {
     resetCode();
   }
 }
@@ -85,27 +106,42 @@ void randomize() {
   }
 }
 
-void btnLedPress() {
-  //Serial.println("heelo");
-  if (btnPress < 4) {
-    btnPress = btnPress + 1;
+void btnPressFlash() {
+  if (lastPress == 0) {
+    digitalWrite(BUTTONRED, HIGH);
+    delay(50);
+    digitalWrite(BUTTONRED, LOW);
+  } else if (lastPress == 1) {
+    digitalWrite(BUTTONGREEN, HIGH);
+    delay(50);
+    digitalWrite(BUTTONGREEN, LOW);
+  } else if (lastPress == 2) {
+    digitalWrite(BUTTONBLUE, HIGH);
+    delay(50);
+    digitalWrite(BUTTONBLUE, LOW);
+  } else if (lastPress == 3) {
+    digitalWrite(BUTTONWHITE, HIGH);
+    delay(50);
+    digitalWrite(BUTTONWHITE, LOW);
   }
-  
-  int color = analogRead(5);
-  
-  Serial.print("red ");
-  Serial.println(color);
+}
 
-  /*
-  if (red == HIGH) {
+void btnLedPress() {
+  color = analogRead(5);
+
+  if (color <= 979 && color >= 975) {
     lastPress = 0;
-  } else if (green == HIGH) {
+  } else if (color <= 931 && color >= 927) {
     lastPress = 1;
-  } else if (blue == HIGH) {
+  } else if (color <= 984 && color >= 980) {
     lastPress = 2;
-  } else if (white == HIGH) {
+  } else if (color <= 740 && color >= 736) {
     lastPress = 3;
   }
-  */
+
+  if (btnPress < 3) {
+    btnPress = btnPress + 1;
+  }
+
   pressed = true;
 }
